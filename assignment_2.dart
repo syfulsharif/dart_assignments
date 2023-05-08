e96d51496f5d73e1d203de87cb559e36eb5bbb65
Syful Sharif <sharif.syful@gmail.com>
2023-05-08 21:45:11 +0600

    Assignment 02 Added

---
+++ assignment_2.dart
@@ -0,0 +1,24 @@
+void main() {
+  studentGrade("Syful Islam Sharif", 80);
+}
+
+String showGrade(int score) {
+  if (score >= 90 && score <= 100) {
+    return 'A';
+  } else if (score >= 80 && score <= 89) {
+    return 'B';
+  } else if (score >= 70 && score <= 79) {
+    return 'C';
+  } else if (score >= 60 && score <= 69) {
+    return 'D';
+  } else if (score <= 59) {
+    return 'F';
+  } else {
+    return 'Invalid Grade';
+  }
+}
+
+void studentGrade(String name, int number) {
+  String score = showGrade(number);
+  print('$name: $score');
+}
