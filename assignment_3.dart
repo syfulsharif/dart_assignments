bda882e58af3718574e5c5392e083ff43234779b
Syful Sharif <sharif.syful@gmail.com>
2023-05-15 22:27:37 +0600

    Static Variable Initialized problem solved

--- assignment_3.dart
+++ assignment_3.dart
@@ -1,9 +1,51 @@
+void main() {
+  //Task # 1 :create three Car objects
+  Car tesla = Car(brand: 'Tesla', model: 'Y', year: 2020, milesDriven: 15000);
+  Car rivian =
+      Car(brand: 'Rivian', model: 'R1S', year: 2022, milesDriven: 10000);
+  Car audi =
+      Car(brand: 'Audi', model: 'Q-8 e-tron', year: 2021, milesDriven: 5000);
+
+  //Task # 2 : Drive each car a different number of miles using the drive() method
+  tesla.drive(1500);
+  rivian.drive(2500);
+  audi.drive(3500);
+
+  // Task 3 : Print out the brand, model, year, and miles driven for each car
+  //Car # 1
+  print(tesla.getBrand());
+  print(tesla.getModel());
+  print(tesla.getYear());
+  print(tesla.getMilesDriven());
+
+  //Car # 2
+  print(rivian.getBrand());
+  print(rivian.getModel());
+  print(rivian.getYear());
+  print(rivian.getMilesDriven());
+
+  //Car # 3
+  print(audi.getBrand());
+  print(audi.getModel());
+  print(audi.getYear());
+  print(audi.getMilesDriven());
+
+  // Task # 4 : print out the age of each car using the getAge() method
+  print(tesla.getAge());
+  print(rivian.getAge());
+  print(audi.getAge());
+
+  //Task # 5 :  print out the total number of Car objects created using the numberOfCars
+  print(Car.numberOfCars);
+}
+
+//This a Car Class
 class Car {
   late String brand;
   late String model;
   late int year;
   late double milesDriven;
-  static late int numberOfCars;
+  static int numberOfCars = 0;
   //a method that takes a double parameter representing the
   // number of miles driven and adds it to the milesDriven
   double drive(double miles) {
@@ -37,5 +79,22 @@ class Car {
   int getAge() {
     int _currentYear = 2023;
     return _currentYear - year;
+  }
+
+  static int countingCars() {
+    return numberOfCars++;
+  }
+
+  //Class Constructor Method for the Car Class
+  Car(
+      {required String brand,
+      required String model,
+      required int year,
+      required double milesDriven}) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.milesDriven = milesDriven;
+    countingCars();
   }
 }
