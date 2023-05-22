ad3740b688f1a5e6e5571957e5d0d3796e5358ad
Syful Sharif <sharif.syful@gmail.com>
2023-05-22 19:39:12 +0600

    Assignment 4 Code Updated

--- assignment_4.dart
+++ assignment_4.dart
@@ -3,7 +3,7 @@ abstract class Account {
   late int accountNumber;
   late double balance;
   double deposit(double amount) {
-    return this.balance + amount;
+    return this.balance = this.balance + amount;
   }
 
   //absract method
@@ -17,6 +17,11 @@ class SavingsAccount extends Account {
   @override
   double withdraw(double amount) {
     return this.balance += (this.balance - amount) * interstRate;
+  }
+
+  SavingsAccount(int accountNumber, double balance, this.interstRate) {
+    this.accountNumber = accountNumber;
+    this.balance = balance;
   }
 }
 
@@ -32,4 +37,33 @@ class CurrentAccount extends Account {
     }
     return this.balance;
   }
+
+  CurrentAccount(int accountNumber, double balance, this.overdraftLimit) {
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+  }
 }
+
+void main() {
+  //Initializing Savings Account
+  SavingsAccount mySB = SavingsAccount(1101, 25000.0, .05);
+  mySB.deposit(5000);
+  print(mySB.balance);
+  mySB.withdraw(20000.0);
+  print(mySB.balance);
+  //Initializing Current Account
+  CurrentAccount myCD = CurrentAccount(2001, 100000.0, 200000.0);
+  myCD.deposit(30000.0);
+  print(myCD.balance);
+  myCD.withdraw(500000.0);
+  myCD.withdraw(20000.0);
+  print(myCD.balance);
+}
+
+
+// ● Create an instance of the SavingsAccount class by providing values for the account
+// number, initial balance, and interest rate.
+// ● Use the instance to perform operations like depositing and withdrawing money.
+// ● Create an instance of the CurrentAccount class by providing values for the account
+// number, initial balance, and overdraft limit.
+// ● Use the instance to perform operations like depositing and withdrawing.
