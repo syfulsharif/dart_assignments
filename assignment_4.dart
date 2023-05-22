d80ed4d8e789777853a40a2ae9f62defbf1fc4d9
Syful Sharif <sharif.syful@gmail.com>
2023-05-22 19:22:23 +0600

    Current Account Added

--- assignment_4.dart
+++ assignment_4.dart
@@ -9,3 +9,27 @@ abstract class Account {
   //absract method
   double withdraw(double amount);
 }
+
+// Define a class SavingsAccount that extends the Account class
+class SavingsAccount extends Account {
+  late double interstRate;
+
+  @override
+  double withdraw(double amount) {
+    return this.balance += (this.balance - amount) * interstRate;
+  }
+}
+
+// Define a class CurrentAccount that extends the Account class.
+class CurrentAccount extends Account {
+  late double overdraftLimit;
+  @override
+  double withdraw(double amount) {
+    if (amount <= this.overdraftLimit) {
+      return this.balance = this.balance - amount;
+    } else {
+      print('insufficient fund');
+    }
+    return this.balance;
+  }
+}
